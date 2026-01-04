09f03e42d4a67bf03f51243933b6ce8827010855
Zeehan Hoque <shafayet.amin.zeehan@g.bracu.ac.bd>
2026-01-04 12:39:33 +0600

    removed files

--- CSE471-main_20-dec 2/client/src/components/ProtectedRoute.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
-import { AuthContext } from '../context/AuthContext';
-
-export default function ProtectedRoute() {
-  const { user } = useContext(AuthContext);
-  return user ? <Outlet /> : <Navigate to="/login" />;
-}
--- CSE471-main_20-dec 2/client/src/components/RoleGuard.js
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-import { AuthContext } from '../context/AuthContext';
-
-export default function RoleGuard({ role, children }) {
-  const { user } = useContext(AuthContext);
-  return user?.role === role ? children : <div>Access denied</div>;
-}
